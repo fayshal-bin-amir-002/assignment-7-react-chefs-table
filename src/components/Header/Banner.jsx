5ed4f839e6c5f16f1ebe1499889911570239e99e
fayshal69 <foyshalbinamir@gmail.com>
2024-03-16 01:02:54 +0600

    updated banner section

--- src/components/Header/Banner.jsx
+++ src/components/Header/Banner.jsx
@@ -3,9 +3,9 @@ import BG from '../../assets/bg.jpg'
 const Banner = () => {
     return (
         <div style={{
-            borderRadius: '24px', objectFit: 'cover', backgroundSize:'cover', 
-            background: `linear-gradient(0deg, rgba(21, 11, 43, 0.90) 0%, rgba(21, 11, 43, 0.00) 100%), url(${BG}) lightgray 0px -18.896px / 100% 123.31% no-repeat`}}>
-            <div className="w-full lg:w-[60%] mx-auto py-14 lg:py-[130px] px-5 text-center text-white">
+            borderRadius: '24px', objectFit: 'cover', backgroundSize:'fit', backgroundRepeat:'no-repeat', 
+            background: `linear-gradient(0deg, rgba(21, 11, 43, 0.90) 0%, rgba(21, 11, 43, 0.00) 100%), url(${BG}) lightgray 0px -18.896px / 100% 123.31%`}}>
+            <div className="w-full lg:w-[60%] mx-auto py-14 lg:pt-[130px] lg:pb-[260px] px-5 text-center text-white">
                 <h1 className="text-2xl lg:text-5xl font-bold mb-6">Discover an exceptional cooking class tailored for you!</h1>
                 <p>Explore personalized cooking classes designed just for you, offering an exceptional culinary experience tailored to your preferences!</p>
                 <div className='space-x-5 lg:space-x-10 mt-5 lg:mt-10 text-xl font-semibold'>
